4136695aaec98381f66e04c9696752d1399f7008
Satyam <nikamsatyam187@gmail.com>
2025-11-16 07:47:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+  // trim leading spaces first
+  str = str.trim();
+
+  // find the index of the first space
+  const index = str.indexOf(" ");
+
+  // if no space found, return entire string
+  if (index === -1) return str;
+
+  // return substring up to the first space
+  return str.slice(0, index);
 }
 
 // Do not change the code below
